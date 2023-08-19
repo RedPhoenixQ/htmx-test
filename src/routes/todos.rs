cef4f1010cd18be3f1b00b16e3bb496da2438304
RedPhoenixQ <steve.dark.03@gmail.com>
2023-08-19 18:31:07 +0200

    Reset form on sumbit and store todos in Vec

--- src/routes/todos.rs
+++ src/routes/todos.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use std::sync::{Arc, Mutex};
 
 use axum::{extract::State, routing::*, Form, Router};
 use html_node::{
@@ -21,10 +18,10 @@ pub struct Todo {
 }
 
 #[derive(Debug, Clone)]
-pub struct Todos(Arc<Mutex<HashMap<u32, Todo>>>);
+pub struct Todos(Arc<Mutex<Vec<Todo>>>);
 
 impl Todos {
-    fn new(todos: HashMap<u32, Todo>) -> Self {
+    fn new(todos: Vec<Todo>) -> Self {
         Self(Arc::new(Mutex::new(todos)))
     }
 }
@@ -46,31 +43,23 @@ struct Check {
 }
 
 pub fn todos_router() -> Router {
-    let mut todos = HashMap::new();
-    todos.insert(
-        0,
+    let todos = vec![
         Todo {
             id: 0,
             title: "Test title".to_string(),
             done: false,
         },
-    );
-    todos.insert(
-        1,
         Todo {
             id: 1,
             title: "Maybe tit".to_string(),
             done: false,
         },
-    );
-    todos.insert(
-        2,
         Todo {
             id: 2,
             title: "To be done".to_string(),
             done: true,
         },
-    );
+    ];
     Router::new()
         .route(
             "/todo",
@@ -88,20 +77,16 @@ async fn add_todo(State(todos): State<Todos>, Form(form): Form<TodoForm>) -> Nod
     dbg!(&todos);
 
     let mut todos_locked = todos.0.lock().unwrap();
-    let id = todos_locked.keys().max().unwrap_or(&0) + 1;
+    let id = todos_locked.iter().map(|t| t.id).max().unwrap_or(0) + 1;
     let todo = Todo {
         id,
         title: form.title,
         done: false,
     };
     let row = single_todo_row(&todo);
-    let form = todo_form();
 
-    todos_locked.insert(id, todo);
-    html!((hx)
-        {form}
-        {row}
-    )
+    todos_locked.push(todo);
+    row
 }
 
 async fn check_todo(
@@ -110,7 +95,7 @@ async fn check_todo(
     Form(form): Form<Check>,
 ) -> Result<Node, ()> {
     dbg!("Is htmx request", htmx, &form);
-    if let Some(todo) = todos.0.lock().unwrap().get_mut(&form.id) {
+    if let Some(todo) = todos.0.lock().unwrap().iter_mut().find(|t| t.id == form.id) {
         todo.done = form.checked.is_some();
         Ok(single_todo_row(&todo))
     } else {
@@ -120,13 +105,16 @@ async fn check_todo(
 
 async fn remove_todo(State(todos): State<Todos>, htmx: Htmx, Form(id): Form<Id>) {
     dbg!("Is htmx request", htmx, &id);
-    todos.0.lock().unwrap().remove_entry(&id.id);
-    dbg!(todos);
+    let mut locked_todos = todos.0.lock().unwrap();
+    if let Some((i, _)) = locked_todos.iter().enumerate().find(|(_, t)| t.id == id.id) {
+        locked_todos.remove(i);
+    }
+    dbg!(&locked_todos);
 }
 
 async fn get_todos(htmx: Htmx, State(todos): State<Todos>) -> Node {
     dbg!("Is htmx request", &htmx);
-    let table = todos_table(todos.0.as_ref().lock().unwrap().values());
+    let table = todos_table(todos.0.lock().unwrap().iter());
     let form = todo_form();
 
     let node = html!((hx)
@@ -145,7 +133,12 @@ async fn get_todos(htmx: Htmx, State(todos): State<Todos>) -> Node {
 
 fn todo_form() -> Node {
     html!((hx)
-        <form id="todo-form" hx-post="/todo" hx-target="next table tbody" hx-swap="afterbegin" hx-swap-oob="true">
+        <form id="todo-form"
+            hx-post="/todo"
+            hx-target="next table tbody"
+            hx-swap="afterbegin"
+            hx-on="htmx:afterRequest: if (!event?.detail?.failed) this.reset()"
+        >
             <label>
                 <input type="text" name="title" />
             </label>
